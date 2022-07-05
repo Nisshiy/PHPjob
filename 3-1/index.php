f722b3e316e2b255f740fff5c9a2f0c6c9bcb18e
Nisshiy <35899941+Nisshiy@users.noreply.github.com>
2022-07-05 15:13:31 +0900

    課題提出テスト_FizzBuzz

---
+++ 3-1/index.php
@@ -0,0 +1,16 @@
+<?php
+//FizzBuzz問題
+
+for ($i=1;$i<=100;$i++){
+    if ($i%3==0 && $i%5==0){
+        echo "FizzBuzz!!";
+    } elseif ($i%3==0){
+        echo "Fizz!";
+    } elseif ($i%5==0){
+        echo "Buzz!";
+    } else {
+        echo $i;
+    }
+    echo "<br>";
+}
+?>
